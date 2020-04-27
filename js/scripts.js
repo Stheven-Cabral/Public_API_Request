059a835cef86b88c1de64a03be90da7e039ae5be
Stheven Cabral <stheven.cabral@gmail.com>
2020-04-26 22:56:17 -0700

    04262020  2256

--- js/scripts.js
+++ js/scripts.js
@@ -5,8 +5,24 @@
  */
 
 
+/***
+ * `randomUsers
+ */
+
+ let randomUsers = [];
+
+
 /**
  * Public Fetch request to the random user API. 
  */
 
-fetch('https://randomuser.me/api/?results=12&format=json').then(results => {console.log(results.json())});
+fetch('https://randomuser.me/api/?results=12')
+.then(users => users.json())
+.then(data => {
+    for (let i = 0; i < data.results.length; i += 1) {
+        randomUsers.push(data.results[i]);
+    }
+    console.log(randomUsers);
+});
+
+
