f246bc29b5f9c96fb806c608649f9edcaa00b4b3
Stheven Cabral <stheven.cabral@gmail.com>
2020-04-27 00:05:33 -0700

    04272020 0005

--- js/scripts.js
+++ js/scripts.js
@@ -10,6 +10,7 @@
  */
 
  let randomUsers = [];
+ const galleryContainer = document.getElementById('gallery');
 
 
 /**
@@ -19,9 +20,23 @@
 fetch('https://randomuser.me/api/?results=12')
 .then(users => users.json())
 .then(data => {
+    let galleryContent = ``;
     for (let i = 0; i < data.results.length; i += 1) {
         randomUsers.push(data.results[i]);
+        // Make into a function instead.
+        galleryContent += `<div class="card">
+                           <div class="card-img-container">
+                           <img class="card-img" src="https://placehold.it/90x90" alt="profile picture">
+                           </div>
+                           <div class="card-info-container">
+                             <h3 id="name" class="card-name cap">first last</h3>
+                             <p class="card-text">email</p>
+                             <p class="card-text cap">city, state</p>
+                           </div>
+                        </div>`
+
     }
+    // Remove once complete.
     console.log(randomUsers);
 });
 
